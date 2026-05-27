b727face0ed85a45a90a3d9a37a92cd5e7139296
sumit-kumar-sahu86 <sumitkumarsahu86@gmail.com>
2026-05-27 18:42:27 +0530

    Add files via upload

---
+++ premitiveDataTypeAssignment.js
@@ -0,0 +1,14 @@
+let firstName ="Sumit"
+console.log("The data type of firstname is", typeof firstName)
+
+let companyName = "TestLeaf"
+console.log("The data type of companyName is", typeof companyName)
+
+let mobileNumber = 8759384775
+console.log("The data type of mobileNumber is", typeof mobileNumber)
+
+let isAutomation = true
+console.log("The data type of isAutomation is", typeof isAutomation)
+
+let hasPlaywrite
+console.log("The data type of hasPlaywrite is", typeof hasPlaywrite)
---
+++ var-let-constAssignment.js
@@ -0,0 +1,17 @@
+
+const browserVersion = "Chrome"
+
+function getBrowserVersion(){
+    if(browserVersion == "Chrome"){
+        
+    var browserVersion = "Local Chrome Version 1"
+    console.log("The var Local Version is", browserVersion)
+
+    let browserVersion = "Local Chrome Version 2"
+    console.log("The let Local Version is", browserVersion)}
+
+    console.log("Outside block Const Global Version is", browserVersion)
+    }
+    getBrowserVersion();
+
+    
